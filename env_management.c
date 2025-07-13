2c39ded8678e679517e9c97c68ef61e8650ad6b2
robotsamurai <maedayukimi@icloud.com>
2025-07-13 16:15:01 +0900

    final gachi last

--- env_management.c
+++ env_management.c
@@ -6,7 +6,7 @@
 /*   By: mawako <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 20:19:30 by mawako            #+#    #+#             */
-/*   Updated: 2025/07/13 15:41:34 by mawako           ###   ########.fr       */
+/*   Updated: 2025/07/13 16:14:49 by mawako           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,8 @@ char	*my_getenv(const char *name, t_env *env)
 	len = ft_strlen(name);
 	while (env->g_env[i])
 	{
-		if (ft_strncmp(env->g_env[i], name, len) == 0 && env->g_env[i][len] == '=')
+		if (ft_strncmp(env->g_env[i], name,
+				len) == 0 && env->g_env[i][len] == '=')
 			return (env->g_env[i] + len + 1);
 		i++;
 	}
@@ -84,7 +85,8 @@ int	my_unsetenv(const char *name, t_env *env)
 	len = ft_strlen(name);
 	while (env->g_env[i])
 	{
-		if (ft_strncmp(env->g_env[i], name, len) == 0 && env->g_env[i][len] == '=')
+		if (ft_strncmp(env->g_env[i], name,
+				len) == 0 && env->g_env[i][len] == '=')
 		{
 			free(env->g_env[i]);
 			while (env->g_env[j])
